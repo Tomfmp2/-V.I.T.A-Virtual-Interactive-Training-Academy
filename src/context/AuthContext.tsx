a58d091d2ba44ee2e966fc1e13315df39e37768f
Andres Felipe Navas Alvear <136179236+craftube23@users.noreply.github.com>
2026-08-10 11:56:03 -0500

    fix: Necessary modifications have been uploaded.

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -11,18 +11,21 @@ interface AuthState {
     token: string | null;
     isAuthenticated: boolean;
     login: (token: string, user: User) => void;
+    logout: () => void;
 }
 
-export const userAuthStore = create<AuthState>((set) => ({
-    user: null,
+export const useAuthStore = create<AuthState>((set) => ({
+    user: JSON.parse(localStorage.getItem('user') || 'null'),
     token: localStorage.getItem('token'),
     isAuthenticated: !!localStorage.getItem('token'),
     login: (token, user) => {
         localStorage.setItem('token', token);
+        localStorage.setItem('user', JSON.stringify(user));
         set({ token, user, isAuthenticated: true});
     },
     logout: () => {
         localStorage.removeItem('token');
+        localStorage.removeItem('user');
         set({ token: null, user: null, isAuthenticated: false });
     },
 }));
